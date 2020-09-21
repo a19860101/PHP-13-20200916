4449f9b590ce9620ab1857f2dd5f08da09a2548a
Zac <a19860101@gmail.com>
2020-09-21 19:21:04 +0800

    20200921

---
+++ condition.php
@@ -0,0 +1,43 @@
+<?php
+    // if(條件){
+    //     動作
+    // }
+
+    // if(條件){
+    //     動作A
+    // }else{
+    //     動作B
+    // }
+
+    // if(條件A){
+    //     動作A
+    // }elseif(條件B){
+    //     動作B
+    // }
+
+    // if(條件A){
+    //     動作A
+    // }elseif(條件B){
+    //     動作B
+    // }else{
+    //     動作C
+    // }
+    $x = 0;
+    if($x > 0){
+        echo "Success 1";
+    }
+
+    if($x < 0){
+        echo "Success 2";
+    }else{
+        echo "Error";
+    }
+
+    if($x > 0){
+        echo "正數";
+    }elseif($x < 0){
+        echo "負數";
+    }else{
+        echo "Error!!!!";
+    }
+
