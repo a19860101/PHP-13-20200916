f51e177b95773b45dbc19fd2767a0403bfb5792f
Zac <a19860101@gmail.com>
2020-09-23 19:32:48 +0800

    20200923

--- string.php
+++ string.php
@@ -4,4 +4,19 @@
     // echo $s;
     // echo substr($s,0,13);
     // echo mb_substr($s,0,3);
-    echo mb_substr($s,0,5,"utf-8");
+    // echo mb_substr($s,0,5,"utf-8");
+
+    $pw = "jieck325@_";
+    echo $pw;
+    echo "<br>";
+    echo md5($pw);
+    echo "<br>";
+    echo sha1($pw);
+    echo "<br>";
+    echo md5(md5($pw));
+    echo "<br>";
+    echo sha1(sha1($pw));
+    echo "<br>";
+    echo md5(sha1($pw));
+    echo "<br>";
+    echo sha1(md5($pw));
