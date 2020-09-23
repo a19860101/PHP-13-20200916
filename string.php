e6ffc987f502fef7bdc7f73ff5dd37a4a12e817c
Zac <a19860101@gmail.com>
2020-09-23 19:23:29 +0800

    20200923

---
+++ string.php
@@ -0,0 +1,7 @@
+<?php
+    $s = "莫瑞肘擊吞惡意犯規 詹姆斯：他不是故意";
+    $s_en = "hello world";
+    // echo $s;
+    // echo substr($s,0,13);
+    // echo mb_substr($s,0,3);
+    echo mb_substr($s,0,5,"utf-8");
