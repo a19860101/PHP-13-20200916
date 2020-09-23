0644806ffcef76d507439c3156bc3a96ada8cb43
Zac <a19860101@gmail.com>
2020-09-23 21:11:52 +0800

    20200923

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="response.php" method="post">
+        <input type="text" name="user">
+        <input type="text" name="mail">
+        <input type="text" name="phone">
+        <input type="submit" value="送出">
+    </form>
+</body>
+</html>
---
+++ response.php
@@ -0,0 +1,12 @@
+<?php
+    // var_dump($_POST); 
+    // echo "<br>";
+    // echo $_POST["user"];
+    // echo "<br>";
+    // echo $_POST["mail"];
+    // echo "<br>";
+    // echo $_POST["phone"];
+    foreach($_POST as $data){
+        echo $data;
+        echo "<br>";
+    }
