6f389049487a1b94b4092c41aa90e457dcc57e7b
Zac <a19860101@gmail.com>
2020-09-30 20:26:10 +0800

    20200930

--- database/conn.php
+++ database/conn.php
@@ -4,5 +4,7 @@
     $db_pw = "admin";
     $db_name = "php1320200916";
 
-    // $conn = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
-    $conn = mysqli_connect("localhost","admin","admin","php1320200916");
+    $conn = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('資料庫連線錯誤，請洽管理員');
+    // $conn = mysqli_connect("localhost","admin","admin","php1320200916");
+
+    mysqli_query($conn,"SET NAMES utf8");
