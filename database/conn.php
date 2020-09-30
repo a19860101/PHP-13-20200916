e965f0d17d4d5eaaada56d49d5a528da570b1cca
Zac <a19860101@gmail.com>
2020-09-30 20:08:33 +0800

    20200930

---
+++ database/conn.php
@@ -0,0 +1,8 @@
+<?php
+    $db_host = "localhost";
+    $db_user = "admin";
+    $db_pw = "admin";
+    $db_name = "php1320200916";
+
+    // $conn = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
+    $conn = mysqli_connect("localhost","admin","admin","php1320200916");
