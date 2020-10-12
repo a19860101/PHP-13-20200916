8e962914250a39b080c707423df5ef0b298091e1
Zac <a19860101@gmail.com>
2020-10-12 19:39:11 +0800

    20201012

--- pdo/index.php
+++ pdo/index.php
@@ -1,3 +1,16 @@
+<?php
+    try {
+        require_once("pdo.php");
+        $sql = "SELECT * FROM students";
+        //預備陳述式
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        
+    }
+    catch(PDOException $e){
+        echo $e->getMessage();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +19,8 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php while($row = $stmt->fetch()){?>
+        <div><?php echo $row["name"];?></div>
+    <?php }?>
 </body>
 </html>
--- pdo/pdo.php
+++ pdo/pdo.php
@@ -8,8 +8,8 @@
     
 
     try {
-        $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
-        // $dsn = "mysql:host=localhost;dbname=php1320200916;charset=utf8mb4";
+        // $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
+        $dsn = "mysql:host=localhost;dbname=php1320200916;charset=utf8mb4";
     
         $pdo = new PDO($dsn,$db_user,$db_pw);
         //相當於$conn
