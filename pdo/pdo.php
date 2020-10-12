f1d23a8f56e14665ac4dfb107f02f225c0c201e8
Zac <a19860101@gmail.com>
2020-10-12 19:13:27 +0800

    20201012

---
+++ pdo/pdo.php
@@ -0,0 +1,13 @@
+<?php
+    $db_host = "localhost";
+    $db_user = "admin";
+    $db_pw = "admin";
+    $db_name = "php1320200916";
+    $db_charset = "utf8mb4";
+
+    $dsn = "mysql:host={$db_host};dbname{$db_name};charset={$db_charset}";
+    // $dsn = "mysql:host=localhost;dbname=php1320200916;charset=utf8mb4";
+
+    $pdo = new PDO($dsn,$db_user,$db_pw);
+    //相當於$conn
+
