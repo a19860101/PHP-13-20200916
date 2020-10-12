28e4a97fadbceace37798562d20d2ee355d08adc
Zac <a19860101@gmail.com>
2020-10-12 21:36:26 +0800

    20201012

--- pdo/index.php
+++ pdo/index.php
@@ -47,7 +47,10 @@
                         <td>
                             <a href="show.php?id=<?php echo $row["id"];?>" class="btn btn-primary">檢視</a>
                             <a href="edit.php?id=<?php echo $row["id"];?>" class="btn btn-success">編輯</a>
-                        
+                            <form action="delete.php" method="post" class="d-inline-block">
+                                <input type="hidden" name="id" value="<?php echo $row["id"];?>">
+                                <input type="submit" value="刪除" class="btn btn-danger" onclick="return confirm('確認刪除？')">
+                            </form>
                         </td>
                     </tr>
                 <?php }?>
