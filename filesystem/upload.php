0fe0e9fe1cadf95df74efec076cb4074bb24c4f7
Zac <a19860101@gmail.com>
2020-10-14 20:28:18 +0800

    20201014

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -12,8 +12,18 @@
     // echo $_FILES["img"]["size"];
 
     extract($_FILES["img"]);
-    echo $name;
-    echo $type;
-    echo $tmp_name;
-    echo $error;
-    echo $size;
+    // echo $name;
+    // echo $type;
+    // echo $tmp_name;
+    // echo $error;
+    // echo $size;
+    $target = "images/".$name;
+    if($error === 0){
+        if(move_uploaded_file($tmp_name,$target)){
+            echo "上傳成功";
+        }else{
+            echo "上傳失敗";
+        }
+    }else{
+        echo "上傳錯誤";
+    }
