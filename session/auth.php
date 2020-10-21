117d7e57c86716850e1b56f08c1672ede1795608
Zac <a19860101@gmail.com>
2020-10-21 20:00:29 +0800

    20201021

--- session/auth.php
+++ session/auth.php
@@ -3,8 +3,19 @@
 
     extract($_POST);
 
-    $_SESSION["USER"] = $user;
-    $_SESSION["PW"] = $pw;
+    // $_SESSION["USER"] = $user;
+    // $_SESSION["PW"] = $pw;
 
-    echo $_SESSION["USER"]."你好!您的密碼是:".$_SESSION["PW"];
-    header("refresh:3;url=index.php");
+    
+    // echo $_SESSION["USER"]."你好!您的密碼是:".$_SESSION["PW"];
+    // header("refresh:3;url=index.php");
+
+    $_SESSION["AUTH"] = $_POST;
+    extract($_SESSION["AUTH"]);
+    // var_dump($_SESSION["AUTH"]);
+    // echo $_SESSION["AUTH"]["user"];
+    // echo "<br>";
+    // echo $_SESSION["AUTH"]["pw"];
+
+    // echo $_SESSION["AUTH"]["user"]."你好!您的密碼是:".$_SESSION["AUTH"]["pw"];
+    echo $user."你好!您的密碼是:".$pw;
