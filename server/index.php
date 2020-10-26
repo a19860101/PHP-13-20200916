d3424ace8093bda93384c0870d813dab2dfaf230
Zac <a19860101@gmail.com>
2020-10-26 20:16:15 +0800

    20201026

--- server/index.php
+++ server/index.php
@@ -10,3 +10,7 @@
     echo "<br>";
     $domain = $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
     echo $domain;
+    echo "<br>";
+    echo $_SERVER["HTTP_REFERER"];
+    echo "<br>";
+    echo $_SERVER["HTTP_USER_AGENT"];
