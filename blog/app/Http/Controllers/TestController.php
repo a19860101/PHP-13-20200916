0b2fdded3ba0589eb8a4a068f0d77c7c2a6f8769
Zac <a19860101@gmail.com>
2020-10-28 21:59:49 +0800

    20201028

--- blog/app/Http/Controllers/TestController.php
+++ blog/app/Http/Controllers/TestController.php
@@ -8,7 +8,9 @@ class TestController extends Controller
 {
     //
     function show($id){
-        return view('test',['id'=>$id]);
+        // return view('test',['id'=>$id]);
+        // return view('test')->with(['id'=>$id]);
+        return view('test',compact('id'));
     }
     function wow($id){
         // return 'WOW!!!';
