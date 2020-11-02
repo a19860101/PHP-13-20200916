7d9e3ffae703b335e96221c2f145266b61415776
Zac <a19860101@gmail.com>
2020-11-02 21:06:01 +0800

    20201102

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
@@ -10,7 +11,14 @@ class PostController extends Controller
     function create(){
         return view('post.create');
     }
-    function store(){
-        return 'store';
+    function store(Request $request){
+        DB::insert('INSERT INTO posts(title,content,created_at,updated_at)VALUES(?,?,?,?)',[
+            $request->title,
+            $request->content,
+            now(),
+            now()
+        ]);
+
+        return '文章已建立';
     }
 }
