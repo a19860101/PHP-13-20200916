484d3c21ba794073a11dea878cf03a6a6a03ef94
Zac <a19860101@gmail.com>
2020-11-11 21:45:17 +0800

    20201111

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,6 +12,7 @@
 */
 
 Route::get('/','PostController@index')->name('post.index');
+
 // Route::get('post/create','PostController@create')->name('post.create');
 // Route::post('post/','PostController@store')->name('post.store');
 // Route::get('post/{id}','PostController@show')->name('post.show');
@@ -19,7 +20,14 @@ Route::get('/','PostController@index')->name('post.index');
 // Route::get('post/{id}/edit','PostController@edit')->name('post.edit');
 // Route::put('post/{id}','PostController@update')->name('post.update');
 
-Route::resource('/post','PostController');
+// Route::resource('/post','PostController')->middleware('auth');
+
+Route::group(['middleware' => 'auth'],function(){
+    Route::resource('/post','PostController')->except('index','show');
+});
+
+Route::resource('/post','PostController')->only('index','show');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
