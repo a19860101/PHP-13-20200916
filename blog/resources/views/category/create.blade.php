d14278a5def48ce388f5c67452685b8f185576b5
Zac <a19860101@gmail.com>
2020-11-16 19:47:45 +0800

    20201116

--- blog/resources/views/category/create.blade.php
+++ blog/resources/views/category/create.blade.php
@@ -1,4 +1,24 @@
 @extends('template.master')
+
 @section('main')
-<h1>Create Category</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+            <form action="{{route('category.store')}}" method="post">
+            @csrf
+                <div class="form-group">
+                    <label for="title">分類標題</label>
+                    <input type="text" class="form-control" id="title" name="title">
+                </div>
+                <div class="form-group">
+                    <label for="slug">分類slug</label>
+                    <input type="text" class="form-control" id="slug" name="slug">
+                </div>
+                <input type="submit" value="建立分類" class="btn btn-primary">
+                <input type="button" value="取消" class="btn btn-danger" onclick="history.back()">
+            </form>
+        </div>
+    </div>
+</div>
+
 @endsection
