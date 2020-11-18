baef9fe1021dddedf5b3877bd94fa457ce699085
Zac <a19860101@gmail.com>
2020-11-18 19:06:51 +0800

    20201118

---
+++ blog/database/migrations/2020_11_18_190425_add_cover_to_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCoverToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->string('cover')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->dropColumn('cover');
+        });
+    }
+}
