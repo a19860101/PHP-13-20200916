3c25915e31e5ddcc49e533ccd43c755579bf9ed1
Zac <a19860101@gmail.com>
2020-11-23 21:43:47 +0800

    20201123

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -155,6 +155,7 @@ class PostController extends Controller
         $post->category_id = $request->category_id;
         $post->save();
 
+        $post->tags()->detach();
         $tags = explode(',',$request->tag);
         foreach($tags as $tag){
             $t = Tag::firstOrCreate(['title'=> $tag]);
