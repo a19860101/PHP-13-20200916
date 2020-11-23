465e26e5f115684b8dc0b6c0df08aebaffb10f8f
Zac <a19860101@gmail.com>
2020-11-23 19:27:28 +0800

    20201123

--- blog/database/migrations/2020_11_23_190827_create_tags_table.php
+++ blog/database/migrations/2020_11_23_190827_create_tags_table.php
@@ -15,6 +15,7 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
             $table->timestamps();
         });
     }
---
+++ blog/database/migrations/2020_11_23_191909_create_post_tag_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('tag_id');
+            $table->timestamps();
+
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('post_tag', function (Blueprint $table) {
+            $table->dropForeign(['post_id']);
+            $table->dropForeign(['tag_id']);
+        });
+        
+        Schema::dropIfExists('post_tag');
+    }
+}
