0f7d3e86502ce14c955b93711ad347ec05b01f31
Zac <a19860101@gmail.com>
2020-11-25 19:23:22 +0800

    20201125

--- blog/app/Post.php
+++ blog/app/Post.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Post extends Model
 {
     //
+    use SoftDeletes;
     protected $fillable = ['title','content'];
 
     public function user(){
